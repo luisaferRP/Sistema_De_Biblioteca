bad04d5e21537390c6af094553ef9cdda3997988
luisaferRP <luisaramiresporras103@gmail.com>
2024-07-18 11:45:55 -0500

    Feat: se crean atributos y metodos de la clase Libro

--- models/Libro.cs
+++ models/Libro.cs
@@ -1,12 +1,40 @@
 using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace SistemaBiblioteca.models
 {
-    public class Libro
+    public class Libro : Publicacion
     {
-        
+        public Guid ISBN;
+        public string Autor;
+        public string Genero;
+        public double Precio;
+
+
+        //Constructor
+        public Libro(string titulo,DateOnly añoPublicacion,Guid isbn,string autor, string genero,double precio)
+        {
+            this.Titulo =titulo;
+            this.AñoPublicacion = añoPublicacion;
+            this.ISBN = isbn;
+            this.Autor = autor;
+            this.Genero = genero;
+            this.Precio = precio; 
+        }
+
+        //Metodo Descripcion
+        public void Descripcion()
+        {
+            Console.WriteLine(@$"
+            --------------------------------
+                    Datos del libro:
+            
+            Titulo : {Titulo}
+            Autor: {Autor}
+            Genero : {Genero}
+            Precio : {Precio}
+            ");
+
+        }
     }
 }
