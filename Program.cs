83a6c2dd1fb4b68f66bdc7f585dadac1c0769fd9
luisaferRP <luisaramiresporras103@gmail.com>
2024-07-18 12:29:18 -0500

    Feat: se hace simulacion de creacion para el objeto libro

--- Program.cs
+++ Program.cs
@@ -2,6 +2,7 @@
 using SistemaBiblioteca.models;
 
 var libro1 = new Libro("La rebelion",new DateOnly (2000,02,12),"Fernando Soto","Novela",25.000);
+
 var biblioteca = new Biblioteca();
 
 biblioteca.ListaLibros.Add(libro1);
